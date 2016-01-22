bfe87e232ba862f3ae672e603ad3d2c22364bb7a
Alexander Harding <alexander.harding@johnsonfit.com>
2016-01-22 16:36:59 -0600

    Readme and testing

--- src/angular-ladda-lw.js
+++ src/angular-ladda-lw.js
@@ -1,30 +1,34 @@
-angular.module('ladda-lw', ['ngAnimate']).directive('ladda', function() {
+angular.module('ladda-lw', ['ngAnimate']).directive('ladda', function laddaDirective() {
   return {
     restrict: 'A',
     scope: {
-      ladda: '='
+      ladda: '=',
     },
-    compile: function(element, attrs) {
-      var lLoading = attrs['ladda'];
+    compile: (element, attrs) => {
+      const lLoading = attrs.ladda;
 
       element.addClass('ladda-lw');
 
-      var textWrap = angular.element('<div class="ladda-lw__text" ng-class="{\'ladda-lw__text--up\': ' + lLoading + '}"></div>');
+      const textWrap = angular.element(`
+        <div class="ladda-lw__text"
+             ng-class="{\'ladda-lw__text--up\': ' + lLoading + '}">
+        </div>
+      `);
 
       textWrap.append(element.contents());
       element.append(textWrap);
 
-      var loadingWrap = angular.element('<div class="ladda-lw__loading-wrap"></div>');
-      var loading = angular.element('<div class="ladda-lw__loading" ng-class="{\'ladda-lw__loading--up\': ' + lLoading + '}"></div>');
+      const loadingWrap = angular.element('<div class="ladda-lw__loading-wrap"></div>');
+      const loading = angular.element('<div class="ladda-lw__loading" ng-class="{\'ladda-lw__loading--up\': ' + lLoading + '}"></div>');
 
       loadingWrap.append(loading);
       element.append(loadingWrap);
 
-      return function link(scope, iElement, iAttrs) {
-        scope.$watch('ladda', function(l) {
+      return function link(scope, iElement) {
+        scope.$watch('ladda', function laddaWatch(l) {
           iElement.attr('disabled', l ? 'disabled' : false);
         });
-      }
-    }
-  }
+      };
+    },
+  };
 });
