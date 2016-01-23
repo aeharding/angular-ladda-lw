68a2ddda9ffd9005391793b69caae47c72789bd1
Alexander Harding <alexander.harding@johnsonfit.com>
2016-01-22 18:42:57 -0600

    Fix for templates in es6

--- src/angular-ladda-lw.js
+++ src/angular-ladda-lw.js
@@ -11,15 +11,19 @@ angular.module('ladda-lw', ['ngAnimate']).directive('ladda', function laddaDirec
 
       const textWrap = angular.element(`
         <div class="ladda-lw__text"
-             ng-class="{\'ladda-lw__text--up\': ' + lLoading + '}">
+             ng-class="{'ladda-lw__text--up': ${lLoading}}">
         </div>
       `);
 
       textWrap.append(element.contents());
       element.append(textWrap);
 
-      const loadingWrap = angular.element('<div class="ladda-lw__loading-wrap"></div>');
-      const loading = angular.element('<div class="ladda-lw__loading" ng-class="{\'ladda-lw__loading--up\': ' + lLoading + '}"></div>');
+      const loadingWrap = angular.element(`<div class="ladda-lw__loading-wrap"></div>`);
+      const loading = angular.element(`
+        <div class="ladda-lw__loading"
+             ng-class="{'ladda-lw__loading--up': ${lLoading}}">
+        </div>
+      `);
 
       loadingWrap.append(loading);
       element.append(loadingWrap);
